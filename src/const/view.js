f832cd27b90f1d4559c3b2f4e72f9fb1d60744d5
Mishan <miha.go.hard@gmail.com>
2022-12-11 17:17:26 +0300

    Разделяет код на компоненты

---
+++ src/const/view.js
@@ -0,0 +1,19 @@
+export const PointTemplateType = {
+  EDIT: 0,
+  ADD_SIMPLE: 1,
+  ADD_WITHOUT_OFFERS: 2,
+  ADD_WITHOUT_DESTINATION: 3,
+};
+
+export const EventsListTemplateType = {
+  EMPTY: 0,
+  FILL: 1,
+  LOADING: 2
+};
+
+export const RenderPosition = {
+  BEFOREBEGIN: 'beforebegin',
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+  AFTEREND: 'afterend',
+};  
